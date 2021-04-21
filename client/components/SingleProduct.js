bbf5f0b2f006f1b574d9a26227384d3ea8d60c80
Nathan Damico <nathandamico5@gmail.com>
2021-04-21 18:05:03 -0400

    Updated view to show all info and add to cart button

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,29 +1,38 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import { fetchProduct } from '../store/singleProduct'
+import React from "react";
+import { connect } from "react-redux";
+import { addToCart } from "../store/cart";
+import { fetchProduct } from "../store/singleProduct";
 
 class SingleProduct extends React.Component {
-
-componentDidMount() {
-  const id = this.props.match.params.productId;
-  this.props.fetchProduct(id);
-  console.log('mounted!')
+  componentDidMount() {
+    const id = this.props.match.params.productId;
+    this.props.fetchProduct(id);
   }
-  render(){
+
+  addToCart() {
+    console.log("added to cart");
+  }
+
+  render() {
+    const { selectedProduct } = this.props;
     return (
       <div>
-        Look! its {this.props.selectedProduct.name}
+        <img src={selectedProduct.imageUrl} />
+        <h2>{selectedProduct.name}</h2>
+        <p>{selectedProduct.description}</p>
+        <button onClick={this.addToCart}>Add To Cart</button>
       </div>
-    )
+    );
   }
 }
 
 const mapState = (state) => ({
   selectedProduct: state.selectedProduct,
-})
+});
 
 const mapDispatch = (dispatch) => ({
   fetchProduct: (id) => dispatch(fetchProduct(id)),
-})
+  addToCart: (productId) => dispatch(addToCart(productId)),
+});
 
-export default connect(mapState, mapDispatch)(SingleProduct)
+export default connect(mapState, mapDispatch)(SingleProduct);
