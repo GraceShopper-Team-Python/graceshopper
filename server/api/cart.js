418cf6c356e8b02fe7bd73d128d62afc7201a38c
Dennese Sanay <dsanay@gmail.com>
2021-04-21 15:42:29 -0400

    finalized cart route and thunk

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,25 +1,28 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  models: { Order , OrderProduct},
-} = require("../db");
+  models: { Order, OrderProduct },
+} = require('../db');
 module.exports = router;
 
 // GET /api/cart/:userId
-router.get("/cart/:userId", async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    const cartOrder = Order.findOne({
+    const cartOrder = await Order.findOne({
       where: {
         userId: req.params.userId,
         isCart: true,
       },
     });
+
     //query orderProducts table
-    const cartProducts = OrderProduct.findAll({
+    const cartProducts = await OrderProduct.findAll({
       where: {
-        orderId: cartOrder.id
+        orderId: cartOrder.id,
       },
-    })
-    res.send(cartProducts)
+      attributes: ['productId', 'quantity'],
+    });
+
+    res.send(cartProducts);
   } catch (error) {
     next(error);
   }
