7a92a855835f84dea741a0bf2386264a24c65dfb
Nathan Damico <nathandamico5@gmail.com>
2021-04-22 17:53:14 -0400

    Added security to users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,25 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   models: { User },
-} = require('../db');
-const requireToken = require('../auth/authMiddleware')
+} = require("../db");
+const requireToken = require("../auth/authMiddleware");
 
 //get /api/users
-router.get('/',  async (req, res, next) => {
+router.get("/", requireToken, async (req, res, next) => {
   try {
-    if(req.user.isAdmin){
-       const users = await User.findAll({
-         // explicitly select only the id and username fields - even though
-         // users' passwords are encrypted, it won't help if we just
-         // send everything to anyone who asks!
-         attributes: ['id', 'username'],
-       });
-       res.json(users);
-    }
-    else {
-      const err = new Error ('Permission Denied')
-      err.status = 403
-      throw err
+    if (req.user.isAdmin) {
+      const users = await User.findAll({
+        // explicitly select only the id and username fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ["id", "username"],
+      });
+      res.json(users);
+    } else {
+      const err = new Error("Permission Denied");
+      err.status = 403;
+      throw err;
     }
   } catch (err) {
     next(err);
