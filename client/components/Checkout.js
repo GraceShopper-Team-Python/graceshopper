355250fcee18e17fc240efa63dadc9601a84797e
Nathan Damico <nathandamico5@gmail.com>
2021-04-27 15:28:00 -0400

    added checkout gifs

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { snakePuns } from "../miscellany/jokes";
+import { checkoutGifs } from "../miscellany/gifs";
 
 const snakeJokes = snakePuns[Math.floor(Math.random() * snakePuns.length)];
 const snakeJokes1 = snakePuns[Math.floor(Math.random() * snakePuns.length)];
 const snakeJokes2 = snakePuns[Math.floor(Math.random() * snakePuns.length)];
+const checkoutGif =
+  checkoutGifs[Math.floor(Math.random() * checkoutGifs.length)];
 
 export default function Checkout() {
   return (
-    <div className="offset">
-      <h4>Checked Out 👀</h4>
-      <h4>{snakeJokes}</h4>
-      <h4>{snakeJokes1}</h4>
-      <h4>{snakeJokes2}</h4>
+    <div className="confirmation offset">
+      <div>
+        <h2>Order Submitted</h2>
+        <h4>{snakeJokes}</h4>
+        <h4>{snakeJokes1}</h4>
+        <h4>{snakeJokes2}</h4>
+        <img className="gif" src={checkoutGif} />
+      </div>
     </div>
   );
 }
